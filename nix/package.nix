cb1a858d8c259bd5af55fb4ef906b9a0908e338d
Travers McInerney <687974+tmcinerney@users.noreply.github.com>
2026-08-24 11:31:33 -0700

    chore(release): prepare v0.1.2 (#8)

--- nix/package.nix
+++ nix/package.nix
@@ -6,7 +6,7 @@
 
 rustPlatform.buildRustPackage {
   pname = "beckon";
-  version = "0.1.1";
+  version = "0.1.2";
   src = ../.;
   cargoLock.lockFile = ../Cargo.lock;
   nativeBuildInputs = [ makeWrapper ];
